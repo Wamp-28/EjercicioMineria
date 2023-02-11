eff3bd71c9c4f18fde8241c8778b9fc103b43c20
William Alexander Matallana <wmatallana28@gmail.com>
2023-02-10 19:28:21 -0500

    Cuarto Commit

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
     public static void main(String[] args) {
 
         System.out.println("Hola Clase");
-
+        System.out.println("Linea 1");
+        System.out.println("Linea 2");
 
     }
 
