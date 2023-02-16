7b01c182c0c47f87684999a0b21e5703cf0c5309
zip-b205-e-009 <zip-b205-e-009@UMD.LOCAL.EDU>
2023-02-15 20:18:13 -0500

    Presentacion Jonathan Triviño

--- src/Principal/Presentacion_Jonathan.java
+++ src/Principal/Presentacion_Jonathan.java
@@ -5,7 +5,7 @@ import java.sql.SQLOutput;
 public class Presentacion_Jonathan {
     public static void main(String[] args) {
 
-        System.out.println("Jonathan Stiven Triviño Castro");
+        System.out.println("Jonathan Stiven Triviño gis");
         System.out.println("Vereda San Jorge");
         System.out.println("Videojuegos");
     }
