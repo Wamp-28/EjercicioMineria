79021aaad69e0f16427773047711935375729013
William Alexander Matallana <wmatallana28@gmail.com>
2023-02-15 19:50:24 -0500

    Ajuste

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -4,9 +4,8 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola Clase");
-        System.out.println("Linea 1");
-        System.out.println("Linea 2");
+        System.out.println("Mineria de datos");
+
 
     }
 
