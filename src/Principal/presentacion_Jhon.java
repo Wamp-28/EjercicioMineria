78b36068d2323b6d9c8a27e02891530cbef62ce8
zip-b205-e-010 <zip-b205-e-010@UMD.LOCAL.EDU>
2023-02-15 20:34:40 -0500

    presentacion Jhon

--- src/Principal/presentacion_Jhon.java
+++ src/Principal/presentacion_Jhon.java
@@ -1,2 +1,10 @@
-package Principal;public class presentacion_Jhon {
+package Principal;
+
+public class presentacion_Jhon {
+    public static void main(String[]args){
+        System.out.println("Jhon Alexander Bejarano Bejarano");
+        System.out.println("Cota");
+        System.out.println("Juegos V");
+
+    }
 }
