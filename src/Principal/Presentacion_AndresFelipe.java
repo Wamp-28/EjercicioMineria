2665886b78540723ce2a8cb89f57dc8b8c9d96fb
juliand13cor <julian.diaz-c@uniminuto.edu.co>
2023-02-17 15:54:39 -0500

    a

---
+++ src/Principal/Presentacion_AndresFelipe.java
@@ -0,0 +1,9 @@
+package Principal;
+
+public class Presentacion_AndresFelipe {
+    public static void main(String[] args) {
+        System.out.println("Andres Felipe Espitia Espitia");
+        System.out.println("Zipaquirá");
+        System.out.println("Tocar la guitarra");
+    }
+}
---
+++ src/Principal/Presentacion_Wilmer_Moncada.java
@@ -0,0 +1,16 @@
+package Principal;
+
+public class Presentacion_Wilmer_Moncada {
+
+    public static void main(String[] args) {
+
+        System.out.println("Wilmer Danilo Moncada Parra");
+        System.out.println("Zipaquira");
+        System.out.println("Estudiante");
+
+
+
+    }
+
+
+}
