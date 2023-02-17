4c736e885c55dbc76c95fa7400bd20a410df41a6
juliand13cor <julian.diaz-c@uniminuto.edu.co>
2023-02-17 15:58:33 -0500

    a

--- src/Principal/presentacion_Jhon.java
+++ src/Principal/presentacion_Jhon.java
@@ -1,10 +1,8 @@
 package Principal;
 
 public class presentacion_Jhon {
-
     public static void main(String[]args){
-        System.out.println("aaaa");   
+        System.out.println("aaaa");
         System.out.println("no sirve");
 
-    
 }
