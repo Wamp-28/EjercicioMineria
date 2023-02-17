55e0adfabc2183f412991108ca7f68735901c9a2
juliand13cor <julian.diaz-c@uniminuto.edu.co>
2023-02-17 15:28:14 -0500

    julian2

--- src/Principal/presentacion_Jhon.java
+++ src/Principal/presentacion_Jhon.java
@@ -2,9 +2,7 @@ package Principal;
 
 public class presentacion_Jhon {
     public static void main(String[]args){
-        System.out.println("Jhon Alexander Bejarano Bejarano");
-        System.out.println("Cota");
-        System.out.println("Juegos V");
+        System.out.println("aaaa");
 
     }
 }
